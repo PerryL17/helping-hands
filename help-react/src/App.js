0f96cb47d5cd11def8197209293de886ed3a621a
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-10-12 16:39:46 -0700

    last

--- help-react/src/App.js
+++ help-react/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Login from "./Login";
 import Register from "./Register";
+import Listing from "./Listing";
 
 function App() {
   return (
     <main className="App">
       <h1 className="logo">Helping Hands</h1>
-      <h3 className="logo">community board</h3>
+      <h3 className="logo">Community Board</h3>
       <Register />
     </main>
   );
